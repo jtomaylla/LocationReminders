0df100b78ff487b7b298e9e465ce5d1af2d3eed8
juantomaylla <juan.tomaylla@gmail.com>
2022-03-02 11:33:38 -0500

    First Commit to Share with Github

---
+++ app/src/main/java/com/udacity/project4/locationreminders/data/ReminderDataSource.kt
@@ -0,0 +1,25 @@
+package com.udacity.project4.locationreminders.data
+
+import androidx.lifecycle.LiveData
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
+
+interface ReminderDataSource {
+
+    fun observeReminders(): LiveData<Result<List<ReminderDTO>>>
+
+    suspend fun refreshReminders()
+
+    fun observeReminder(id: String): LiveData<Result<ReminderDTO>>
+
+    suspend fun refreshReminder(id: String)
+
+    suspend fun getReminders(): Result<List<ReminderDTO>>
+
+    suspend fun saveReminder(reminder: ReminderDTO)
+
+    suspend fun getReminder(id: String): Result<ReminderDTO>
+
+    suspend fun deleteAllReminders()
+
+}
